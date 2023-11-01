38cd0f74492127c9c3d464b5e6368e899c28f7cc
Sid <saytleader@outlook.com>
2023-11-01 09:12:55 -0400

    Update Sypm.hx

--- src/Sypm.hx
+++ src/Sypm.hx
@@ -1,7 +1,22 @@
 class Sypm {
     public static function unpack(tar:String) {}
 
-    public static function install(dir:String) {}
+    public static function genflist(dir:String) {
+        var flist = [];
 
-    public static function remove(flist:String) {}
+        var contents = FileSystem.readDirectory(dir);
+        for (item in contents) {
+            if (FileSystem.isDirectory(item)) {
+                flist = flist.concat(genflist(dir + "/" + item));
+            } else {
+                flist.push(dir + "/" + item);
+            }
+        }
+
+        return flist;
+    }
+
+    public static function install(srcdir:String, destdir:String, flist:Array<String>) {}
+
+    public static function remove(dir:String, flist:Array<String>) {}
 }
